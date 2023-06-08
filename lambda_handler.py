c8b81bc10da1b9aa62e527eb0f28712542538f55
saikumar <sai999694@gmail.com>
2023-06-08 14:24:40 +0530

    minor update : updated lambda handler function

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,20 +1,9 @@
-from mangum import Mangum
+import Mangum as Mangum
 
-from analysis import process_sqs_analysis_event
-from dashboard.app import app
+from app import app
 
 mangum_app = Mangum(app=app)
 
 
 def lambda_handler(event: dict, context):
-    is_sqs = event.get('Records', None) is not None and len(event['Records']) > 0
-    if is_sqs:
-        records = event.get('Records')
-        for _record in records:
-            _body = _record['body']
-            print(_body)
-            process_sqs_analysis_event(_body)
-        return {'statusCode': 200, 'body': 'event processed success', 'headers': {"content-type": 'application/json'}}
-    else:
-        response = mangum_app(event, context)
-        return response
+    return mangum_app(event, context)
